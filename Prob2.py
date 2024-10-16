d09d8f6cec609231996554ed73cd06e6ed9d07ef
sgsandoval <sgsandoval@willamette.edu>
2024-10-15 20:35:38 -0700

    Update Prob2.py

--- Prob2.py
+++ Prob2.py
@@ -1,5 +1,5 @@
 ########################################
-# Name:
+# Name: sonora
 # Collaborators:
 # Estimated time spent (hrs):
 ########################################
@@ -20,20 +20,21 @@ def draw_pyramid():
     gw = GWindow(WIDTH, HEIGHT)
 
     # You got it from here
+win_width = BRICK_WIDTH * BRICKS_IN_BASE
+    win_height = BRICK_HEIGHT * (BRICKS_IN_BASE + 1) // 2
+    start_x = (win_width // 2) - (BRICK_WIDTH // 2)
+    start_y = win_height - (BRICK_HEIGHT // 2)
+    
+    for row in range(BRICKS_IN_BASE):
+        num_bricks = BRICKS_IN_BASE - row
+        y_position = start_y - (row * BRICK_HEIGHT)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+        x_start = start_x - (row * (BRICK_WIDTH // 2))
+        
+        for brick in range(num_bricks):
+            x_position = x_start + (brick * BRICK_WIDTH)
+            brick_rect = GRect(x_position, y_position, BRICK_WIDTH, BRICK_HEIGHT)
+            gw.add(brick_rect)
 
 
 
